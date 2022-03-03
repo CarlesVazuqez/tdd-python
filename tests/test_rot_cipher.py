98a12d5358b77bc856bb3715a2f0c8bd3902b56e
Ciber <carlesvazquezinf2@gmail.com>
2022-03-03 20:29:39 +0100

    cifrado rot13

--- tests/test_rot_cipher.py
+++ tests/test_rot_cipher.py
@@ -3,6 +3,6 @@ from app.rot13_cipher import rot13_cipher
 
 class TestClass:
     def test_rot13_cipher(test):
-        assert rot13_cipher("hello friend") == "uryyb sevraq"
-        assert rot13_cipher("hola amigo") == "ubyn nzvtb"
-        assert rot13_cipher("prueba de rot13") == "cehron qr ebg13"
+        assert rot13_cipher("hellofriend") == "uryybsevraq"
+        assert rot13_cipher("holaamigo") == "ubynnzvtb"
+        assert rot13_cipher("pruebaderot") == "cehronqrebg"
